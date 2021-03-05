12a05f8fb62f2deb67fcee28252ed362bc87a67d
= <=>
2021-03-04 21:00:01 -0500

    Added flavor text functionality

--- playgame.py
+++ playgame.py
@@ -1,10 +1,18 @@
 import navigation
 import newworld
+import random
 
 class Maus:
     def __init__(self, *coordinates):
         self.loc = list(coordinates)
 
-worldDimensions = input('Number of dimensions: ')
-worldLength = input('World size in cells: ')
+worldDimensions = int(input('Number of dimensions: '))
+worldLength = int(input('World size in cells: '))
 world = newworld.newWorld(worldDimensions, worldLength)
+
+def win():
+    with open('foods.txt', 'r') as foodsFile:
+        foodsList = [x for x in foodsFile]
+    food = foodsList[random.randrange(len(foodsList))]
+    food = food.rstrip('\n')
+    print(f'The mouse finds {food} and scarfs it down. Good job!')
