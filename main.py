035bb50a96a99e62e9a0f5d7e6b7f7f364e4eb18
= <=>
2021-03-05 21:51:10 -0500

    Converted to class methods

--- main.py
+++ main.py
@@ -8,9 +8,45 @@ class Maus:
             self.loc[i] = int(worldSize / 2) # placing the maus in the middle of the world
         self.worldDimensions = worldDimensions
         self.worldSize = worldSize
-        self.goal = newGoal(worldDimensions, worldSize)
+        self.goal = generateGoal(worldDimensions, worldSize)
 
-def distance(pointA = tuple, pointB = tuple):
+    def getAddress(self):
+        worldDimensions = self.worldDimensions
+        addressList = []
+        for i in range(worldDimensions):
+            addressList.append(self.loc[i])
+        addressTuple = tuple(addressList)
+        return addressTuple
+
+    def move(self, movement = dict): # returns velocity
+        oldAddress = self.getAddress()
+        for i in range(self.worldSize):
+            self.loc[i] += movement[i]
+        newAddress = self.getAddress()
+        velocity = getVelocity(self.goal, oldAddress, newAddress)
+        return velocity
+
+    def moveDown(self):
+        return self.moveOneInTwoDimensions(1, -1)
+
+    def moveLeft(self):
+        return self.moveOneInTwoDimensions(0, -1)
+
+    def moveRight(self):
+        return self.moveOneInTwoDimensions(0, 1)
+
+    def moveOneInTwoDimensions(self, dimension, distance):
+        dimensions = self.worldDimensions
+        movement = dict()
+        for i in dimensions:
+            movement[i] = 0
+        movement[dimension] = distance
+        return self.move(movement)
+
+    def moveUp(self):
+        return self.moveOneInTwoDimensions(1, 1)
+
+def getDistance(pointA = tuple, pointB = tuple):
     if len(pointA) == len(pointB):
         distances = []
         for index, dummy in enumerate(pointA):
@@ -21,56 +57,20 @@ def distance(pointA = tuple, pointB = tuple):
     else:
         return None
 
-def getMausAddress(maus):
-    worldDimensions = maus.worldDimensions
-    addressList = []
-    for i in range(worldDimensions):
-        addressList.append(maus.loc[i])
-    addressTuple = tuple(addressList)
-    return addressTuple
-
 def getVelocity(goal, oldAddress, newAddress):
-    oldDistance = distance(oldAddress, goal)
-    newDistance = distance(newAddress, goal)
+    oldDistance = getDistance(oldAddress, goal)
+    newDistance = getDistance(newAddress, goal)
     velocity = abs(oldDistance - newDistance)
     return velocity
 
-def move(maus, movement = dict): # returns velocity
-    oldAddress = getMausAddress(maus)
-    for i in range(maus.worldSize):
-        maus.loc[i] += movement[i]
-    newAddress = getMausAddress(maus)
-    velocity = getVelocity(maus.goal, oldAddress, newAddress)
-    return velocity
-
-def moveDown(maus):
-    return moveOneInTwoDimensions(maus, 1, -1)
-
-def moveLeft(maus):
-    return moveOneInTwoDimensions(maus, 0, -1)
-
-def moveRight(maus):
-    return moveOneInTwoDimensions(maus, 0, 1)
-
-def moveOneInTwoDimensions(maus, dimension, distance):
-    dimensions = maus.worldDimensions
-    movement = dict()
-    for i in dimensions:
-        movement[i] = 0
-    movement[dimension] = distance
-    return move(maus, movement)
-
-def moveUp(maus):
-    return moveOneInTwoDimensions(maus, 1, 1)
-
-def newGoal(worldDimensions, worldSize):
+def generateGoal(worldDimensions, worldSize):
     addressList = []
     for dummy in range(worldDimensions):
         addressList.append(random.randrange(worldSize))
     addressTuple = tuple(addressList)
     return addressTuple
 
-def win():
+def eatFood():
     with open('foods.txt', 'r') as foodsFile:
         foodsList = [x for x in foodsFile]
     food = foodsList[random.randrange(len(foodsList))]
