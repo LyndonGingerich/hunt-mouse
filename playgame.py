27a7edac8cf22388fe48f015418d0712a9f7eb33
= <=>
2021-03-05 20:37:51 -0500

    Added newGoal(); eliminated newworld.py

--- playgame.py
+++ playgame.py
@@ -12,3 +12,10 @@ def win():
     food = foodsList[random.randrange(len(foodsList))]
     food = food.rstrip('\n')
     print(f'The mouse finds {food} and scarfs it down. Good job!')
+
+def newGoal(worldDimensions, worldSize):
+    addressList = []
+    for dummy in range(worldDimensions):
+        addressList.append(random.randrange(worldSize))
+    addressTuple = tuple(addressList)
+    return addressTuple
