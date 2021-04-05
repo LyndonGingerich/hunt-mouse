57138bfc14cd5821be9ca14907c9bfe97aeed30d
Lyndon Gingerich <gundericusthemighty@gmail.com>
2021-04-05 18:01:38 -0500

    Updated unit tests

--- test/test_main.py
+++ test/test_main.py
@@ -9,13 +9,10 @@ from gridmaus.gridmaus import main
 
 # Test code
 
-def test_adjust_to_boundaries():
-    assert main.adjust_to_boundaries(1, 5) == 1
-    assert main.adjust_to_boundaries(-1, 5) == 0
-    assert main.adjust_to_boundaries(7, 5) == 5
-
-def test_get_difference():
-    assert main.get_difference(10, 7) == 3
+def adjust_coordinate_to_boundary():
+    assert main.adjust_coordinate_to_boundary(1, 5) == 1
+    assert main.adjust_coordinate_to_boundary(-1, 5) == 0
+    assert main.adjust_coordinate_to_boundary(7, 5) == 5
 
 def test_get_distance():
     assert main.get_distance((0, 0, 1), (0, 0, 0)) == 1
@@ -23,9 +20,9 @@ def test_get_distance():
     assert main.get_distance((0, 0, 0), (0, 0, 0)) == 0
 
 def test_get_velocity():
-    assert main.get_velocity(goal=(0, 0, 0), address1=(0, 0, 1), address2=(0, 0, 2)) == -1
-    assert main.get_velocity(goal=(0, 0, 0), address1=(0, 0, 3), address2=(0, 0, 2)) == 1
-    assert main.get_velocity(goal=(0, 2, 0), address1=(0, 1, 0), address2=(0, 3, 0)) == 0
+    assert main.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 1), to_address=(0, 0, 2)) == -1
+    assert main.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 3), to_address=(0, 0, 2)) == 1
+    assert main.get_velocity(goal=(0, 2, 0), from_address=(0, 1, 0), to_address=(0, 3, 0)) == 0
 
 def test_rand_range():
     assert 0 <= main.rand_range(5) < 5
