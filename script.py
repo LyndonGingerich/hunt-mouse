e91ce0f9a009c57bd348ec06af4a88d333f224a0
Lyndon Gingerich <gundericusthemighty@gmail.com>
2021-04-14 15:09:07 -0500

    Began work on playing by script

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-from gridmaus import main
+'''Modify this file to play Gridmaus by script.'''
+
+import subprocess
+
+PROCESS = subprocess.call(['python.exe', 'main.py'], stdout=subprocess.PIPE)
+
+print(PROCESS)
