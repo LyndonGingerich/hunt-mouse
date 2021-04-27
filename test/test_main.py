ec3c07af2013d8c96556f8068f9cffab0f5d1bf0
Lyndon Gingerich <gundericusthemighty@gmail.com>
2021-04-27 13:06:38 -0500

    Updated to match current file names

--- test/test_main.py
+++ test/test_main.py
@@ -2,27 +2,24 @@
 
 import pytest
 
-from gridmaus import main
+import game
 
 # Test code
 
 def adjust_coordinate_to_boundary():
-    assert main.adjust_coordinate_to_boundary(1, 5) == 1
-    assert main.adjust_coordinate_to_boundary(-1, 5) == 0
-    assert main.adjust_coordinate_to_boundary(7, 5) == 5
+    assert game.adjust_coordinate_to_boundary(1, 5) == 1
+    assert game.adjust_coordinate_to_boundary(-1, 5) == 0
+    assert game.adjust_coordinate_to_boundary(7, 5) == 5
 
 def test_get_distance():
-    assert main.get_distance((0, 0, 1), (0, 0, 0)) == 1
-    assert main.get_distance((0, 7, 0), (0, 0, 0)) == 7
-    assert main.get_distance((0, 0, 0), (0, 0, 0)) == 0
+    assert game.get_distance((0, 0, 1), (0, 0, 0)) == 1
+    assert game.get_distance((0, 7, 0), (0, 0, 0)) == 7
+    assert game.get_distance((0, 0, 0), (0, 0, 0)) == 0
 
 def test_get_velocity():
-    assert main.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 1), to_address=(0, 0, 2)) == -1
-    assert main.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 3), to_address=(0, 0, 2)) == 1
-    assert main.get_velocity(goal=(0, 2, 0), from_address=(0, 1, 0), to_address=(0, 3, 0)) == 0
-
-def test_rand_range():
-    assert 0 <= main.rand_range(5) < 5
+    assert game.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 1), to_address=(0, 0, 2)) == -1
+    assert game.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 3), to_address=(0, 0, 2)) == 1
+    assert game.get_velocity(goal=(0, 2, 0), from_address=(0, 1, 0), to_address=(0, 3, 0)) == 0
 
 if __name__ == '__main__':
     pytest.main()
