e6986d610380f7483c22c4b155df97a4188c134e
Lyndon Gingerich <gundericusthemighty@gmail.com>
2021-04-30 15:23:46 -0500

    Added and corrected unit tests

--- test/test_game.py
+++ test/test_game.py
@@ -6,7 +6,10 @@ import game
 
 # Test code
 
-def adjust_coordinate_to_boundary():
+def test_adjust_address_to_boundaries():
+    assert game.adjust_address_to_boundaries((1, 1, 5), 3) == (1, 1, 3)
+
+def test_adjust_coordinate_to_boundary():
     assert game.adjust_coordinate_to_boundary(1, 5) == 1
     assert game.adjust_coordinate_to_boundary(-1, 5) == 0
     assert game.adjust_coordinate_to_boundary(7, 5) == 5
@@ -15,6 +18,7 @@ def test_get_distance():
     assert game.get_distance((0, 0, 1), (0, 0, 0)) == 1
     assert game.get_distance((0, 7, 0), (0, 0, 0)) == 7
     assert game.get_distance((0, 0, 0), (0, 0, 0)) == 0
+    assert game.get_distance((0, 0, 0), (1, 1, 1)) == (2 ** 0.5) ** 3
 
 def test_get_velocity():
     assert game.get_velocity(goal=(0, 0, 0), from_address=(0, 0, 1), to_address=(0, 0, 2)) == -1
