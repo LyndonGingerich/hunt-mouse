f4e7d85acbcd52326580433103d43bff0ae2f848
Lyndon Gingerich <gingerichlyndon@gmail.com>
2023-08-12 09:47:43 -0500

    Rewrite distance and inline abs_difference_of_tuple

--- helpers.py
+++ helpers.py
@@ -7,12 +7,9 @@ def difference(x, y):
     return abs(x - y)
 
 
-def abs_difference_of_tuple(x):
-    return difference(*x)
-
-
 def distance(address1, address2):
-    return hypot(*map(abs_difference_of_tuple, zip(address1, address2)))
+    differences = [difference(coord1, coord2) for coord1, coord2 in zip(address1, address2)]
+    return hypot(*differences)
 
 
 def repeat_tuple(element, length):
